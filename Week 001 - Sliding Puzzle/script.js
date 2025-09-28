441b6a65281cdf773f20d07cc4aa574922c9c0f3
Astremy <43485551+Astremy@users.noreply.github.com>
2025-09-28 16:44:40 +0200

    Week 001 - Sliding Puzzle

---
+++ Week 001 - Sliding Puzzle/script.js
@@ -0,0 +1,134 @@
+
+/* Sources : 
+    https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
+    https://developer.mozilla.org/fr/docs/Web/API/CanvasRenderingContext2D/drawImage
+    https://stackoverflow.com/questions/8912917/cutting-an-image-into-pieces-through-javascript
+    https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+    https://stackoverflow.com/questions/8903854/check-image-width-and-height-before-upload-with-javascript
+    https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+    https://stackoverflow.com/questions/4032179/how-do-i-get-the-width-and-height-of-a-html5-canvas
+*/
+
+const canvasSize = 4;
+const shuffleCount = 200;
+
+let canvasTiles = [];
+
+let image = new Image();
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+class Canvas {
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+
+        let size = canvas.getBoundingClientRect().width;
+        this.tileSize = size / canvasSize;
+        this.image = new Image();
+
+        this.tiles = [];
+        this.moveableTiles = [];
+
+        this.loaded = false;
+    }
+
+    loadImage(img) {
+        this.loaded = false;
+
+        let reader = new FileReader();
+        reader.readAsDataURL(img[0]);
+        reader.onload = function(event) {
+            this.image.src = event.target.result;
+        }.bind(this);
+
+        this.image.onload = function() {
+            let minSize = Math.min(this.image.width, this.image.height);
+            this.imageTile = minSize / canvasSize;
+
+            this.tiles = [];
+
+            for (let i = 0; i < canvasSize*canvasSize; i++) {
+                this.tiles.push(i);
+            }
+
+            this.draw();
+            this.calculateMoveableTiles();
+            this.shuffle();
+            this.loaded = true;
+        }.bind(this);
+    }
+
+    calculateMoveableTiles() {
+        let emptyIndex = this.tiles.indexOf(canvasSize*canvasSize-1);
+        let emptyX = emptyIndex % canvasSize;
+        let emptyY = Math.floor(emptyIndex / canvasSize);
+
+        this.moveableTiles = [];
+        if (emptyX > 0) this.moveableTiles.push(this.tiles[emptyIndex - 1]);
+        if (emptyX < canvasSize - 1) this.moveableTiles.push(this.tiles[emptyIndex + 1]);
+        if (emptyY > 0) this.moveableTiles.push(this.tiles[emptyIndex - canvasSize]);
+        if (emptyY < canvasSize - 1) this.moveableTiles.push(this.tiles[emptyIndex + canvasSize]);
+    }
+
+    moveTile(tile) {
+        if (!this.moveableTiles.includes(tile)) return;
+
+        let tileIndex = this.tiles.indexOf(tile);
+        let emptyIndex = this.tiles.indexOf(canvasSize*canvasSize-1);
+
+        [this.tiles[tileIndex], this.tiles[emptyIndex]] = [this.tiles[emptyIndex], this.tiles[tileIndex]];
+
+        this.draw();
+        this.calculateMoveableTiles();
+    }
+
+    draw() {
+        for (let i=0; i < this.tiles.length; i++) {
+            let tile = this.tiles[i];
+
+            let iCanva = i % canvasSize;
+            let jCanva = Math.floor(i / canvasSize);
+
+            if (tile == canvasSize*canvasSize-1) {
+                this.ctx.fillStyle = "black";
+                this.ctx.fillRect(iCanva * this.tileSize, jCanva * this.tileSize, this.tileSize, this.tileSize);
+                continue;
+            }
+
+            let iImage = tile % canvasSize;
+            let jImage = Math.floor(tile / canvasSize);
+
+            this.ctx.drawImage(this.image, iImage * this.imageTile, jImage * this.imageTile, this.imageTile, this.imageTile,
+                iCanva * this.tileSize, jCanva * this.tileSize, this.tileSize, this.tileSize);
+        }
+    }
+
+    async shuffle() {
+        for (let i = 0; i < shuffleCount; i++) {
+            let randomTile = this.moveableTiles[Math.floor(Math.random() * this.moveableTiles.length)];
+            this.moveTile(randomTile);
+
+            await sleep(1);
+        }
+    }
+}
+
+const canvas = new Canvas(document.getElementById('puzzleCanvas'));
+
+document.getElementById('imageLoader').addEventListener('change', function(e) {
+    canvas.loadImage(e.target.files);
+}, false);
+
+document.getElementById('puzzleCanvas').addEventListener('click', function(e) {
+    if (!canvas.loaded) return;
+
+    let rect = e.target.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    let i = Math.floor(x / canvas.tileSize);
+    let j = Math.floor(y / canvas.tileSize);
+    let tile = canvas.tiles[j * canvasSize + i];
+    canvas.moveTile(tile);
+});
